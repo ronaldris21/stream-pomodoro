f2c5dc3ff94792d72495b94acac21c14f8ab4a79
Ronald Tejada <ronald.tejada@oportunidades.org.sv>
2024-04-15 06:16:28 +0200

    AutoStart Pomodoro

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const urlParams = new URLSearchParams(window.location.search);
 
 const lengths = {
-  pomodoro: (Number(urlParams.get('pomodoro')) || 25) * 60,
-  shortBreak: (Number(urlParams.get('shortBreak')) || 5) * 60,
-  longBreak: (Number(urlParams.get('longBreak')) || 10) * 60,
-}
+  pomodoro: (Number(urlParams.get("pomodoro")) || 0.2) * 60,
+  shortBreak: (Number(urlParams.get("shortBreak")) || 0.1) * 60,
+  longBreak: (Number(urlParams.get("longBreak")) || 0.2) * 60,
+};
 
-const breakSound = new Audio('sounds/break.mp3');
-const workSound = new Audio('sounds/work.mp3');
+const breakSound = new Audio("sounds/break.mp3");
+const workSound = new Audio("sounds/work.mp3");
 
+let counterPomodoros = 0;
+let cantPomodorosLongBreak = 4;
 let mode;
 let interval;
 let length;
@@ -16,63 +18,79 @@ let paused;
 let remainingTime;
 let endTime;
 
-document.querySelector('#mode-buttons').addEventListener('click', function (event) {
-  const { mode: newMode } = event.target.dataset;
-  if (!newMode) return;
+document.getElementById("sessions").textContent = "Sessions: "+ counterPomodoros;
 
+
+function setMode(newMode) {
   mode = newMode;
-
-  document.querySelectorAll('button[data-mode]').forEach(e => e.classList.remove('active'));
-  document.querySelector(`[data-mode="${mode}"]`).classList.add('active');
-
   length = lengths[mode];
+  document
+    .querySelector("#mode-buttons")
+    .querySelectorAll("button")
+    .forEach((button) => {
+      button.classList.remove("active");
+    });
+  document.querySelector(`[data-mode=${mode}]`).classList.add("active");
   startTimer();
-});
+}
 
-document.querySelector('#pause-button').addEventListener('click', function (event) {
-  if (paused) {
-    endTime = Date.now() + remainingTime * 1000;
-    runTimer();
-  } else if (interval) {
-    clearInterval(interval);
-    interval = null;
-    paused = true;
-    document.querySelector('#pause-button').classList.add('active');
-    document.title = 'PAUSED';
-    document.getElementById('text').textContent = 'PAUSED';
-  }
-});
+document
+  .querySelector("#mode-buttons")
+  .addEventListener("click", function (event) {
+    const { mode: newMode } = event.target.dataset;
+    if (!newMode) return;
 
-document.querySelector('#adjust-buttons').addEventListener('click', function (event) {
-  if (paused) {
-    return;
-  }
+    setMode(newMode);
+  });
 
-  const { action } = event.target.dataset;
+document
+  .querySelector("#pause-button")
+  .addEventListener("click", function (event) {
+    if (paused) {
+      endTime = Date.now() + remainingTime * 1000;
+      runTimer();
+    } else if (interval) {
+      clearInterval(interval);
+      interval = null;
+      paused = true;
+      document.querySelector("#pause-button").classList.add("active");
+      document.title = "PAUSED";
+      document.getElementById("text").textContent = "PAUSED";
+    }
+  });
 
-  switch (action) {
-    case "plus":
-      if (interval) {
-        length += 60;
-        endTime += 60000;
-        updateClock();
-      } else {
-        length = 60;
-        startTimer();
-      }
-      break;
-    case "minus":
-      if (interval) {
-        length -= 60;
-        endTime -= 60000;
-        if (endTime < 0) {
-          endTime = 0;
+document
+  .querySelector("#adjust-buttons")
+  .addEventListener("click", function (event) {
+    if (paused) {
+      return;
+    }
+
+    const { action } = event.target.dataset;
+
+    switch (action) {
+      case "plus":
+        if (interval) {
+          length += 60;
+          endTime += 60000;
+          updateClock();
+        } else {
+          length = 60;
+          startTimer();
         }
-        updateClock();
-      }
-      break;
-  }
-});
+        break;
+      case "minus":
+        if (interval) {
+          length -= 60;
+          endTime -= 60000;
+          if (endTime < 0) {
+            endTime = 0;
+          }
+          updateClock();
+        }
+        break;
+    }
+  });
 
 function updateClock() {
   remainingTime = (endTime - Date.now()) / 1000;
@@ -80,33 +98,52 @@ function updateClock() {
     remainingTime = 0;
     clearInterval(interval);
     interval = null;
-    if (mode === 'pomodoro') {
+    if (mode === "pomodoro") {
       breakSound.play();
+      breakSound.play();
+      breakSound.play();
+      counterPomodoros++;
+      document.getElementById("sessions").textContent = "Sessions: "+ counterPomodoros;
+
+      if (counterPomodoros % cantPomodorosLongBreak === 0) {
+        setMode("longBreak");
+      } else {
+        setMode("shortBreak");
+      }
     } else {
       workSound.play();
+      workSound.play();
+      workSound.play();
+      setMode("pomodoro");
     }
   }
 
   const remainingSeconds = Math.round(remainingTime);
-  const minutes = Math.floor(remainingSeconds / 60).toString().padStart(2, '0');
-  const seconds = (remainingSeconds % 60).toString().padStart(2, '0');
+  const minutes = Math.floor(remainingSeconds / 60)
+    .toString()
+    .padStart(2, "0");
+  const seconds = (remainingSeconds % 60).toString().padStart(2, "0");
   const time = `${minutes}:${seconds}`;
 
-  document.getElementById('clock').textContent = time;
+  document.getElementById("clock").textContent = time;
 
-  const text = mode === 'pomodoro' ? 'FOCUS TIME' : 'BREAK TIME';
+  let textBreak =
+    counterPomodoros % cantPomodorosLongBreak === 0
+      ? "LONG BREAK"
+      : "SHORT BREAK";
+  const text = mode === "pomodoro" ? "FOCUS TIME" : textBreak;
   document.title = `${time} - ${text}`;
-  document.getElementById('text').textContent = text;
+  document.getElementById("text").textContent = text;
 
-  const progress = length == 0 ? 1 : ((length - remainingTime) / length)
+  const progress = length == 0 ? 1 : (length - remainingTime) / length;
 
-  document.getElementById('progress-value').style.width = progress * 100 + "vw";
+  document.getElementById("progress-value").style.width = progress * 100 + "vw";
 }
 
 function runTimer() {
   clearInterval(interval);
   paused = false;
-  document.querySelector('#pause-button').classList.remove('active')
+  document.querySelector("#pause-button").classList.remove("active");
   updateClock();
   interval = setInterval(updateClock, 100);
 }
@@ -115,3 +152,12 @@ function startTimer() {
   endTime = Date.now() + length * 1000;
   runTimer();
 }
+
+console.log(lengths);
+console.log(length);
+if (lengths.pomodoro && lengths.longBreak && lengths.shortBreak) {
+  setMode("pomodoro");
+  runTimer();
+}
+
+// startTimer();
